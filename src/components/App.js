04a6b176b9f84b935a8dc864daf06c57e5cbdc47
Olesia Polevoi <olesia.polevoi@gmail.com>
2022-09-15 14:18:52 -0400

    creted basic structure for app

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Chest from "./Chest";
+function App() {
+  return (
+    <div className="App">
+      Hello App
+      <Chest />
+    </div>
+  );
+}
+
+export default App;
